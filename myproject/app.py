650c06a45ce8036e5ebb3e6baf46aecbd7ce2855
ayonadee <ayonaduncan@gmail.com>
2021-03-18 14:07:51 +0000

    final commit before merge

--- myproject/app.py
+++ myproject/app.py
@@ -1,7 +1,7 @@
 from application import app, models, db
 from flask import Flask, render_template,request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, DateField
+from wtforms import StringField, SubmitField, IntegerField, DateField, DateTimeField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lockedout'
@@ -22,7 +22,7 @@ class NetworkForm(FlaskForm):
     contact_number = StringField('Contact Number')
     event = StringField('Event')
     spark = StringField('Spark')
-    meeting_date = DateField('Date')
+    meeting_date = DateTimeField('Date')
     user_id = IntegerField('user id')
     submit = SubmitField('Submit New Contact')
     
@@ -32,7 +32,6 @@ def network(user_id):
     error=""
     form = NetworkForm()
     form.user_id.data = user_id
-    
     if(request.method=='POST'):
         first_name = form.first_name.data
         last_name = form.last_name.data
@@ -53,7 +52,7 @@ def network(user_id):
             last_name = form.last_name.data,email_address = form.email_address.data, 
             company = form.company.data, position = form.position.data,
             social_media_account = form.social_media_account.data,contact_number = form.contact_number.data,
-            event = form.event.data,spark = form.spark.data,meeting_date = form.meeting_date.data, user_id = user_id)
+            event = form.event.data,spark = form.spark.data,meeting_date = form.meeting_date.data , user_id = user_id)
             db.session.add(new_contact)
             db.session.commit()
             return redirect('/contacts')
@@ -87,9 +86,9 @@ def contacts():
     networks = models.Network.query.all()
     return render_template('contacts.html', networks=networks)
 
-@app.route('/deletenetwork/<int:user_id>')
-def delete(user_id):
-    network_to_delete = models.Network.query.filter_by(id=user_id).first()
+@app.route('/deletenetwork/<int:id>')
+def delete(id):
+    network_to_delete = models.Network.query.filter_by(id=id).first()
     db.session.delete(network_to_delete)
     db.session.commit()
     return redirect('/contacts')
@@ -100,18 +99,8 @@ def update(id):
     first_network = models.Network.query.filter_by(id=id).first()
     error=""
     form = NetworkForm()
-    form.user_id.data = first_network.user_id
-    form.first_name.data = first_network.first_name
-    form.last_name.data = first_network.last_name
-    form.email_address.data = first_network.email_address
-    form.company.data = first_network.company
-    form.position.data = first_network.position
-    form.social_media_account.data = first_network.social_media_account
-    form.contact_number.data = first_network.contact_number
-    form.event.data = first_network.event
-    form.spark.data = first_network.spark
-    form.meeting_date.data = first_network.meeting_date
-
+    form.user_id.data = id
+    
     if(request.method=='POST'):
         first_network.first_name = form.first_name.data
         first_network.last_name= form.last_name.data
@@ -123,14 +112,24 @@ def update(id):
         first_network.event = form.event.data
         first_network.spark = form.spark.data
         first_network.meeting_date = form.meeting_date.data
-        first_network.user_id = form.user_id.data
+        
         if len(form.first_name.data) == 0 or len(form.last_name.data) == 0 or len(form.email_address.data) == 0 or len(form.company.data) == 0 or len(form.position.data) == 0 or len(form.social_media_account.data) == 0 or len(form.contact_number.data) == 0  or len(form.event.data) == 0 or len(form.spark.data) == 0 or len(str(form.meeting_date.data)) == 0:
             error = "No fields have been edited"
         else:
             db.session.commit()
             return redirect('/contacts')
-    users = models.Users.query.all()
-    return render_template('network.html', form=form, users=users, message=error) 
+    else:
+        form.first_name.data = first_network.first_name
+        form.last_name.data = first_network.last_name
+        form.email_address.data = first_network.email_address
+        form.company.data = first_network.company
+        form.position.data = first_network.position
+        form.social_media_account.data = first_network.social_media_account
+        form.contact_number.data = first_network.contact_number
+        form.event.data = first_network.event
+        form.spark.data = first_network.spark
+        form.meeting_date.data = first_network.meeting_date
+    return render_template('network.html', form=form, users=users, message1=error) 
    
 
 if __name__ == '__main__':
